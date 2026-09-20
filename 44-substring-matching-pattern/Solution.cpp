248e7bafdb3708901fb14b5a9087b2b93ecdd500
stutii30 <stutisharma3012@gmail.com>
2026-09-20 23:54:35 +0530

    LeetSync: add solution for substring-matching-pattern (v1)

---
+++ 44-substring-matching-pattern/Solution.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+bool isstar(string &s, int k){
+    for(int i=0;i<=k;i++){
+        if(s[i]!='*')
+        return false;
+    }
+    return true;
+}
+bool solve(string &s, string &p, int i, int j,vector<vector<int>>&dp){
+    if(j<0) return true;
+    if(i<0) return isstar(p,j);
+    if(i>=0 and j<0) return false;
+    if(dp[i][j]!=-1) return dp[i][j];
+    if(s[i]==p[j]){
+        return dp[i][j]=solve(s,p,i-1,j-1,dp);
+    
+    }
+    if(p[j]=='*'){
+        return dp[i][j]= solve(s,p,i-1,j,dp) or solve(s,p,i,j-1,dp);
+    }
+    return false;
+
+}
+    bool hasMatch(string s, string p) {
+        int n=s.size();
+        vector<vector<int>>dp(s.size(),vector<int>(p.size(),-1));
+        for(int i=0;i<n;i++){
+            if(solve(s,p,s.size()-1,p.size()-1,dp)) return true;
+        }
+        return false;
+    }
+};
