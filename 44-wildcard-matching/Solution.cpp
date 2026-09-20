8991e6f10b153b24ceb1a65efddc38d7eccfe58a
stutii30 <stutisharma3012@gmail.com>
2026-09-20 16:32:06 +0530

    LeetSync: add solution for wildcard-matching (v1)

---
+++ 44-wildcard-matching/Solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+bool isstar(string &s, int i){
+    for(int j=0;j<=i;j++){
+        if(s[j]!='*')
+        return false;
+    }
+    return true;
+}
+bool solve(string &s, string &p, int i, int j,vector<vector<int>>& dp){
+    if(i<0 and j<0){
+        return true;
+    }
+    if(i<0 and j>=0) return isstar(p,j);
+    if(i>=0 and j<0) return false;
+    if(dp[i][j]!=-1) return dp[i][j];
+    if(s[i]==p[j] or p[j]=='?')
+    dp[i][j]=solve(s,p,i-1,j-1,dp);
+    if(p[j]=='*')
+    return dp[i][j]=solve(s,p,i-1,j,dp) or solve(s,p,i,j-1,dp);
+
+    return false;
+}
+    bool isMatch(string s, string p) {
+        vector<vector<int>>dp(s.size()+1,vector<int>(p.size()+1,-1));
+        return solve(s,p,s.size()-1,p.size()-1,dp);
+    }
+};
